d634229c139bdcc28b360da032cbd7139c3e9267
Agustina de los Santos <fadu.agusdelosantos@gmail.com>
2025-08-10 17:09:11 -0300

    event bubbling

--- eventos.js
+++ eventos.js
@@ -1,5 +1,10 @@
-let div = document.getElementById('contenedor')
+let div = document.getElementById('contenedor');
+let boton = document.querySelector('button');
 
 div.addEventListener('click', function() {
-    alert('Hola! Soy el div')
-})
+    alert('Hola! Soy el div');
+});
+
+boton.addEventListener('click', function(event){
+    event.stopPropagation();
+});
